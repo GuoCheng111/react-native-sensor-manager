a042a52e034586a55a9924b00bb455856ff3119f
guocheng <guocheng@cyclecentury.com>
2019-10-12 15:04:43 +0800

    for react native  0.58.0

--- android/src/main/java/com/sensormanager/SensorManagerPackage.java
+++ android/src/main/java/com/sensormanager/SensorManagerPackage.java
@@ -21,12 +21,7 @@ public class SensorManagerPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
-
-	@Override
-	public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-		return Arrays.<ViewManager>asList();
-	}
 }
